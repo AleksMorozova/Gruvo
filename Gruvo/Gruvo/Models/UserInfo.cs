1be7f85f0ec3685d6112e89019db4e8149cb7577
Lika <lika.shugurova@gmail.com>
2018-07-21 02:09:54 +0300

    added models

---
+++ Gruvo/Gruvo/Models/UserInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gruvo.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        [DataType(DataType.Password)] 
+        public string Password { get; set; }
+        [DataType(DataType.EmailAddress)] 
+        public string Email { get; set; }
+        public string ProfileDesc { get; set; }
+    }
+}
