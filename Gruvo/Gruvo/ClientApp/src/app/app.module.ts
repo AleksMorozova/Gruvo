d33a12db619ee7e54a1e4332082dbcc1dd1f184b
Ivan Paniotov <ivanpaniotov@gmail.com>
2018-07-29 22:00:24 +0300

    deleted some lines

--- Gruvo/Gruvo/ClientApp/src/app/app.module.ts
+++ Gruvo/Gruvo/ClientApp/src/app/app.module.ts
@@ -5,16 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginPageComponent,
-    LoginFormComponent,
-    SignupFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
