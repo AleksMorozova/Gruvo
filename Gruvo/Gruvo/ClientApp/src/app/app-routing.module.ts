f71d527182d80061d0426911a65f82675f0eac07
Ivan Paniotov <ivanpaniotov@gmail.com>
2018-08-01 13:13:28 +0300

    deleted imports
    Co-Authored-By: Mariia Dehtiarenko <mdegtyarenko725@gmail.com>

--- Gruvo/Gruvo/ClientApp/src/app/app-routing.module.ts
+++ Gruvo/Gruvo/ClientApp/src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 //import components and servises here
 
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-
 const appRoutes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
