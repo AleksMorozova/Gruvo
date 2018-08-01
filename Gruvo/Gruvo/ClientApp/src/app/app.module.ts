0d801cc8b2c9d76216d7b4939ed1e62198cb36cb
Ivan Paniotov <ivanpaniotov@gmail.com>
2018-08-01 11:28:54 +0300

    Revert "added readme, removed footer and header components for now"
    This reverts commit 3e638bf400ac87c1e8cee44bd5987f573480c1a6.

--- Gruvo/Gruvo/ClientApp/src/app/app.module.ts
+++ Gruvo/Gruvo/ClientApp/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
