a084213f3dad9b31e7d1f2a78239ddd2a4c3e394
dertosp <38324214+dertosp@users.noreply.github.com>
2018-08-04 10:52:38 +0300

    Deleted unnecessary usings

--- Gruvo/Gruvo/Startup.cs
+++ Gruvo/Gruvo/Startup.cs
@@ -1,18 +1,12 @@
 using Gruvo.BLL;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 namespace Gruvo
 {
