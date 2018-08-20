3742f98bf6059868b3f860114ffa100b482e90ab
dertosp <protos123415@gmail.com>
2018-08-20 22:03:58 +0300

    Added controller and model
    Added TweetController
    Added LikeModel
    Removed extra usings from ReadableTweet

---
+++ Gruvo/Gruvo/Controllers/TweetController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Gruvo.DAL.Repository;
+using Gruvo.DTL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gruvo.Controllers
+{
+    [Authorize(Policy ="GruvoCookie")]
+    [Route("api/[controller]")]
+    public class TweetController : ControllerBase
+    {
+        private BaseRepository _repository;
+
+        public TweetController(BaseRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [Route("Like")]
+        [HttpPost]
+        public IActionResult Like([FromBody] LikeModel like)
+        {
+            try
+            {
+                if(like == null)
+                {
+                    return BadRequest();
+                }
+
+                if(_repository.TweetDAO.Dislike(like.PostId, like.UserId) == 0)
+                {
+                    _repository.TweetDAO.Like(like.PostId, like.UserId);
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Something went wrong");
+            }
+        }
+    }
+}
---
+++ Gruvo/Gruvo/DTL/LikeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Gruvo.DTL
+{
+    public class LikeModel
+    {
+        public long PostId { get; set; }
+        public long UserId { get; set; }
+    }
+}
