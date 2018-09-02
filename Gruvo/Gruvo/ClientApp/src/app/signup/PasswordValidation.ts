7457aef25b20956c8eb205d67576827eb0973cba
Мария Дегтяренко <mdegtyarenko725@gmail.com>
2018-09-02 19:51:42 +0300

    password confirmation added

---
+++ Gruvo/Gruvo/ClientApp/src/app/signup/PasswordValidation.ts
@@ -0,0 +1,9 @@
+import { AbstractControl, FormGroup } from '@angular/forms';
+export class PasswordValidation {
+
+  static PasswordsMatch(form: FormGroup) {
+    let password = form.get('password').value;
+    let confirmPassword = form.get('confirmPassword').value;
+    return password == confirmPassword ? null : { 'PasswordsDoNotMatch': true}
+  }
+}
